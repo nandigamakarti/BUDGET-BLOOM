9a6cb1683f608f87713160780f60403ea3241f44
KARTIKEYA NANDIGAMA <116968353+nandigamakarti@users.noreply.github.com>
2025-06-01 11:11:52 +0530

    New

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// vite.config.ts
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: ["gentle-growth-finance-main.onrender.com"]
+  }
+})
